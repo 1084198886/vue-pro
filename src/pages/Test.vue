5f9c60a3aad74c77729234d91af5fe52815951f1
gengqiyun <gengqiyun@newcapec.net>
2022-04-25 16:00:33 +0800

    Init

---
+++ src/pages/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <main-layout>
+    <p>Test Page</p>
+  </main-layout>
+</template>
+
+<script>
+  import MainLayout from '../layouts/Main.vue'
+
+  export default {
+    components: {
+      MainLayout
+    }
+  }
+</script>
